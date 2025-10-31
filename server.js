35f220417c399a93b18062300d61044f9bbedd7f
sabiridwan <sabi@sam-mediagmail.com>
2025-10-31 15:39:11 +0800

    update server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const __dirname = dirname(__filename);
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8777;
 
 // Middleware
 app.use(express.json());
